d336d6a16ea873d75ad5cd125b5b5b8ae42ca2c5
maximilien0405 <maximilien.zimmermann@ik.me>
2022-08-05 11:11:47 +0200

    fix(app): rename project & build path

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/pixel-ui-public'),
+      dir: require('path').join(__dirname, './coverage/brainsight-ui-public'),
       subdir: '.',
       reporters: [
         { type: 'html' },
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'pixel-ui-public';
+  title = 'brainsight-ui-public';
 }
